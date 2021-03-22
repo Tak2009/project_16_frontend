fa04b982a2ec4ed77687b993dd6f17ad5f339b63
Takeshi Kimoto <lnuk2009jp@gmail.com>
2021-03-22 20:41:37 +0000

    changed

--- src/4_components/NavBar.js
+++ src/4_components/NavBar.js
@@ -7,52 +7,56 @@ const link = {
   width: '100px',
   padding: '1px',
   margin: '0 6px 6px',
-  background: 'blue',
+  // background: 'blue',
   textDecoration: 'none',
-  color: 'white',
-  'border-radius': '5px',
+  color: 'white'
 }
-
-
 
 const NavBar = () => {
   return (
     <div className="navbar">
-      <NavLink 
-        style={link} 
-        exact to="/"
-        activeStyle={{
-          background: "darkblue"
-        }}
-      >Home</NavLink>
-      <NavLink 
-        style={link} 
-        to="/about"
-        activeStyle={{
-          background: "darkblue"
-        }}
-      >About This Website</NavLink>
-      <NavLink 
-        style={link} 
-        to="/pubs"
-        activeStyle={{
-          background: "darkblue"
-        }}
-      >Local Pub List</NavLink>
-      <NavLink 
-        style={link} 
-        to="/pub/registration"
-        activeStyle={{
-          background: "darkblue"
-        }}
-      >For Pub Owner (Registration)</NavLink>
-      <NavLink 
-        style={link} 
-        to="/supporter/registration"
-        activeStyle={{
-          background: "darkblue"
-        }}
-      >For Supporter (Registration)</NavLink>
+      <ul className="header">
+        <li><NavLink 
+            style={link} 
+            exact to="/"
+            // activeStyle={{
+            //   background: "darkblue"
+            // }}
+            >Home</NavLink>
+        </li>
+        <li><NavLink 
+            style={link} 
+            to="/about"
+            // activeStyle={{
+            //   background: "darkblue"
+            // }}
+            >About This Website</NavLink>
+        </li>
+        <li><NavLink 
+            style={link} 
+            to="/pubs"
+            // activeStyle={{
+            //   background: "darkblue"
+            // }}
+            >Local Pub List</NavLink>
+        </li>
+        <li><NavLink 
+            style={link} 
+            to="/pub/registration"
+            // activeStyle={{
+            //   background: "darkblue"
+            // }}
+            >For Pub Owner (Registration)</NavLink>
+        </li>
+        <li><NavLink 
+            style={link} 
+            to="/supporter/registration"
+            // activeStyle={{
+            //   background: "darkblue"
+            // }}
+            >For Supporter (Registration)</NavLink>
+        </li>
+      </ul>
     </div>
   );
 }
