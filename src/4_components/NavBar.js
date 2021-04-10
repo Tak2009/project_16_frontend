6bb5b0fdd54dcc111779dea79daf1c257615a82a
Takeshi Kimoto <lnuk2009jp@gmail.com>
2021-04-10 17:00:05 +0100

    some css chnages to background

--- src/4_components/NavBar.js
+++ src/4_components/NavBar.js
@@ -3,57 +3,42 @@ import { NavLink } from 'react-router-dom';
 import '../App.css';
 
 
-const link = {
-  width: '100px',
-  padding: '1px',
-  margin: '0 6px 6px',
-  // background: 'blue',
-  textDecoration: 'none',
-  color: 'white'
-}
+// const link = {
+//   width: '100px',
+//   padding: '1px',
+//   margin: '0 6px 6px',
+//   // background: 'blue',
+//   textDecoration: 'none',
+//   color: 'white'
+// }
 
 const NavBar = () => {
   return (
     <div className="navbar">
       <ul className="header">
-        <li><NavLink 
-            style={link} 
+        <li className="link"><NavLink 
+            // style={link} 
             exact to="/"
-            // activeStyle={{
-            //   background: "darkblue"
-            // }}
             >Home</NavLink>
         </li>
-        <li><NavLink 
-            style={link} 
+        <li className="link"><NavLink 
+            // style={link} 
             to="/about"
-            // activeStyle={{
-            //   background: "darkblue"
-            // }}
             >About This Website</NavLink>
         </li>
-        <li><NavLink 
-            style={link} 
+        <li className="link"><NavLink 
+            // style={link} 
             to="/pubs"
-            // activeStyle={{
-            //   background: "darkblue"
-            // }}
             >Local Pub List</NavLink>
         </li>
-        <li><NavLink 
-            style={link} 
+        <li className="link"><NavLink 
+            // style={link} 
             to="/pub/registration"
-            // activeStyle={{
-            //   background: "darkblue"
-            // }}
             >For Pub Owner (Registration)</NavLink>
         </li>
-        <li><NavLink 
-            style={link} 
+        <li className="link"><NavLink 
+            // style={link} 
             to="/supporter/registration"
-            // activeStyle={{
-            //   background: "darkblue"
-            // }}
             >For Supporter (Registration)</NavLink>
         </li>
       </ul>
